c2dcf6366f6a8711f196d61da6e0bc30ff513b4c
Kagu <kagaogameplays@gmail.com>
2025-02-01 11:01:46 -0300

    subindo desafios do Ian

---
+++ ian/desafio02.py
@@ -0,0 +1,7 @@
+numero = int(input("digite um numero: "))
+antecessor1 = numero-1
+antecessor2 = numero-2
+sucessor1 = numero+1
+sucessor2 = numero+2
+print("os 2 antecessores desse numero são: ",antecessor1," e ",antecessor2, " e os 2 sucessores são: ", sucessor1," e ",sucessor2,".")
+print
